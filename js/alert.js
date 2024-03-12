eef5ed083a0ad7a55ba11635a6eee96d18f5dcf9
Madiyar <amadiar654@gmail.com>
2024-03-12 14:14:37 +0500

    changed

---
+++ js/alert.js
@@ -0,0 +1,72 @@
+"use strict"
+// let age = prompt('Сколько тебе лет?', '');
+// alert(`Тебе ${age} лет!`);
+
+
+// let name = prompt('What is Your name?', '');
+// age = parseInt(age1) + 1
+// age1 = +age1 + 3
+// name = Number(name) + 1
+// alert(name);
+
+
+// let IsSudent = confirm('Are you student?')
+// alert(IsSudent)
+
+// let num = 'madiyar'
+// alert(typeof num)
+
+// let madiyar = true;
+// alert(typeof madiyar); // boolean
+
+// madiyar = 'madi'; // теперь value это строка "true"
+// alert(typeof madiyar); // string
+
+// alert( "6" / "2" ); // 3, строки преобразуются в числа
+
+// let num = '123'
+// alert(Number(num) + 12)
+
+// alert( "6" / "2" ); // 3, строки преобразуются в числа
+
+// let str = "123";
+// alert(typeof str); // string
+
+// let num = Number(str); // становится числом 123
+
+// alert(typeof num); // number
+
+// alert(Number("   123   ")); // 123
+// alert(Number("123z")); // NaN (ошибка чтения числа на месте символа "z")
+// alert(Number(true)); // 1
+// alert(Number(false)); // 0
+// alert(+true) //
+
+// alert( Boolean(1) ); // true
+// alert( Boolean(0) ); // false
+
+// alert( Boolean("Привет!") ); // true
+// alert( Boolean("") ); // false
+
+// alert( 6 - '2' ); // 4, '2' приводится к числу
+// alert( '6' / '2' ); // 3, оба операнда приводятся к числам
+
+// let a = 1;
+// let b = 2;
+
+// let c = 3 - (a = b + 1);
+
+// alert( a ); // 3
+// alert( c ); // 0
+
+// let counter = 0;
+// ++counter;
+// counter++;
+// alert(counter); // 2, обе строки сделали одно и то же
+
+// let counter = 0;
+// let a = ++counter;
+
+// alert(counter); // 2, обе строки сделали одно и то же
+
+
