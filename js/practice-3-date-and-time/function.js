cdcdf190c42437e0e1f40d663dc58493a2dc751b
madiiyar <amadiar654@gmail.com>
2024-03-17 14:18:38 +0500

    studied theory

---
+++ js/practice-3-date-and-time/function.js
@@ -0,0 +1,58 @@
+// function greet (name) {
+//     console.log('Hello - ' , name);
+// }
+
+// const greet2 = function(name) {
+//     console.log('2 Hello - ' , name);
+// }
+
+// greet('Madiyar')
+// greet2('Madiyar')
+
+// console.dir(greet);
+
+// setTimeout(function () {
+//     greet('Madiyar')
+// }, 1000)
+
+// let counter = 0;
+// const interval1 = setInterval(function() {
+//     // if (counter === 5) {
+//     //     clearInterval(interval1);
+//     // } else {
+//     //     console.log(++counter);
+//     // }
+//     console.log(++counter);
+// }, 100);
+
+// function greet (name) {
+//     console.log('Hello - ' , name);
+// }
+
+// const arrow = (name) => {
+//     console.log('Hello - ' , name);
+// }
+
+// const arrow1 = name => console.log('Hello - ' , name);
+
+// function pow(num, exp) {
+//     return Math.pow(num, exp)
+// }
+
+// const po2 = (num, exp) => Math.pow(num, exp)
+
+// console.log(po2(12, 3));
+// console.log(pow(2,3));
+
+// const sum = (num, num2) => num + num2
+
+// console.log(sum(12, 90));
+
+// function createPerson(name) {
+//     return function(lastname){
+//         console.log(name + ' ' + lastname);
+//     }
+// }
+
+// const addLastName = createPerson('Madiyar')
+// addLastName('Aqylbek1')
